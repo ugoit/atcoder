4c2a0d729b8c888236528094428c7c0d5e8f7a48
ugoit <ykshoyk@gmail.com>
2020-11-02 22:44:03 +0800

    Update abc176C.py

--- abc176C.py
+++ abc176C.py
@@ -1,3 +1,17 @@
-N=list(map(int,input().split()))
-A=[0]*N
+N=int(input())
+A=[""]*N
 A=list(map(int,input().split()))
+
+maxA=A[0]
+cntchair=0
+for i in range(N-1):
+    if maxA>A[i+1]:
+        cntchair+=maxA-A[i+1]
+    elif maxA<A[i+1]:
+        maxA=A[i+1]
+
+print(cntchair)
+
+
+
+
